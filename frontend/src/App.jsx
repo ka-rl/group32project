bd68e03cea8967a476455f52c5344014fb2f6a80
Steban Martinez <martinezsteban01@gmail.com>
2024-09-20 03:00:41 -0500

    created basic landing page with join us button and login button Note: will add functionality to buttons later

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
 import { Button } from "@chakra-ui/react"
 import { Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
 
 
 
 function App() {
   return (
-    <box minH={"100vh"}>
-      {/* <Navbar /> */}
+    <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
       </Routes>
-    </box>
+      </>
   )
 }
 
