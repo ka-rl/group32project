b597013dcb66f60a8be5211b51ca579ee51fa7db
ka-rl <lykarld@gmail.com>
2024-10-18 15:04:34 -0500

    register/login functions w validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 //wont let us save to DB unless these rules are followed
@@ -13,5 +14,49 @@ const userSchema = new Schema ({
         required: true
     }
 })
+
+userSchema.statics.register = async function(email, password) {
+
+    //validation
+    if (!email || !password){
+        throw Error('All fields must be filled')
+    }
+    if (!validator.isEmail(email)){
+        throw Error('Email is invalid')
+    }
+    if (!validator.isStrongPassword(password)){
+        throw Error('Password not strong enough')
+    }
+    
+    const exists = await this.findOne({ email })
+    if (exists) {
+        throw Error('Email already in use')
+    }
+
+    const user = await this.create({ email, password })
+
+    return user
+}
+
+userSchema.statics.login = async function(email, password) {
+    
+    if (!email || !password){
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({ email })
+    if (!user) {
+        throw Error('Incorrect email')
+    }
+
+    const match = (password == user.password)
+    
+    if (!match) {
+        throw Error('Incorrect password')
+    }
+
+    return user
+
+}
 
 module.exports = mongoose.model('User', userSchema)
