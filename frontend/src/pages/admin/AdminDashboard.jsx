bb288773bf1be13186e4daa71373807a8e171ec4
Steban Martinez <martinezsteban01@gmail.com>
2024-11-21 11:50:55 -0600

    added sidebar to admindashboard

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,29 +1,51 @@
 import React from 'react';
-import { ChakraProvider, Box, Flex, Heading, Button, Text, Image } from '@chakra-ui/react';
+import { ChakraProvider, Box, Flex, Heading, Button, Text, VStack } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-import Navbar from '../../components/Navbar';
 
 function AdminDashboard() {
   return (
     <ChakraProvider>
-      <Box bg="gray.100" minH="100vh">\
-        {/* sidebar */}
+      <Box bg="gray.100" minH="100vh">
+        {/* Sidebar */}
         <Flex>
+          <Box
+            as="nav"
+            bg="teal.500"
+            color="white"
+            w="250px"
+            minH="100vh"
+            p="4"
+            position="fixed"
+          >
+            <VStack spacing="6" align="flex-start">
+              <Heading as="h4" size="md" mb="4">
+                Admin Menu
+              </Heading>
+              <Button as={Link} to="/manage-events" variant="ghost" colorScheme="whiteAlpha" w="full">
+                Manage Events
+              </Button>
+              <Button as={Link} to="/event-history" variant="ghost" colorScheme="whiteAlpha" w="full">
+                Event History
+              </Button>
+              <Button as={Link} to="/volunteer-data" variant="ghost" colorScheme="whiteAlpha" w="full">
+                Volunteer Data
+              </Button>
+            </VStack>
+          </Box>
 
-
-        </Flex>
-
-        {/* main page */}
-        <Flex
-          direction="column"
-          align="center"
-          justify="center"
-          minH="70vh"
-          padding="8"
-        >
-          <Heading as="h2" size="2xl" textAlign="center" mb="4">
-            Admin Dashboard
-          </Heading>
+          {/* Main Page */}
+          <Box ml="250px" flex="1" p="8">
+            <Flex
+              direction="column"
+              align="center"
+              justify="center"
+              minH="70vh"
+            >
+              <Heading as="h2" size="2xl" textAlign="center" mb="4">
+                Admin Dashboard
+              </Heading>
+            </Flex>
+          </Box>
         </Flex>
 
         <Box as="footer" bg="teal.500" color="white" padding="4" textAlign="center">
